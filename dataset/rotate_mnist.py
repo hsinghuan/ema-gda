1c6855071589459fb721744cef50a587361bfad7
hsinghuan <hhchung@utexas.edu>
2023-03-16 16:14:32 -0500

    rotate mnist preprocess code

---
+++ dataset/rotate_mnist.py
@@ -0,0 +1,27 @@
+from typing import List
+import numpy as np
+import torch
+from torchvision import datasets, transforms
+import torchvision.transforms.functional as TF
+
+
+class RandomRotation:
+    def __init__(self, range:List):
+        self.range = range
+
+    def __call__(self, x):
+        angle = np.random.uniform(self.range[0], self.range[1])
+        return TF.rotate(x, angle)
+
+
+rotate_mnist_domains = [[0,5],[5,10],[10,15],[15,20],[20,25],[25,30],[30,35],[35,40],[40,45],[45,50],[50,55],[55,60]]
+degree_sum = 60
+
+def get_rotate_mnist(data_dir: str, domain: List, batch_size: int):
+    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,)), RandomRotation(domain)])
+    train_dataset = datasets.MNIST(data_dir, train=True, download=True, transform=transform)
+    train_dataset = torch.utils.data.Subset(train_dataset, range(len(train_dataset) * domain[0] // degree_sum, len(train_dataset) * domain[1] // degree_sum)) # each image will be seen only once
+    train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [int(len(train_dataset) * 0.9), len(train_dataset) - int(len(train_dataset) * 0.9)])
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    return train_loader, val_loader
