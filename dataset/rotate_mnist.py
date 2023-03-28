59c4a33eb43a3728b4a70d335ea64db909cf6aaa
hsinghuan <hhchung@utexas.edu>
2023-03-28 11:35:47 -0500

    add the option to get target test loader

--- dataset/rotate_mnist.py
+++ dataset/rotate_mnist.py
@@ -17,8 +17,13 @@ class RandomRotation:
 rotate_mnist_domains = [[0,5],[5,10],[10,15],[15,20],[20,25],[25,30],[30,35],[35,40],[40,45],[45,50],[50,55],[55,60]]
 degree_sum = 60
 
-def get_rotate_mnist(data_dir: str, domain: List, batch_size: int, val: bool):
+def get_rotate_mnist(data_dir: str, domain: List, batch_size: int, target_test: bool = False, val: bool = True):
     transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,)), RandomRotation(domain)])
+    if target_test:
+        test_dataset = datasets.MNIST(data_dir, train=False, download=True, transform=transform)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+        return test_loader
+
     train_dataset = datasets.MNIST(data_dir, train=True, download=True, transform=transform)
     train_dataset = torch.utils.data.Subset(train_dataset, range(len(train_dataset) * domain[0] // degree_sum, len(train_dataset) * domain[1] // degree_sum)) # each image will be seen only once
     if val:
