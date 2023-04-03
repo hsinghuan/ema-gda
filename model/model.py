4e327ce5370446da9ac5b325941d64068a69d464
hsinghuan <hhchung@utexas.edu>
2023-04-03 16:35:30 -0500

    add a model class to encapsule encoder and head

---
+++ model/model.py
@@ -0,0 +1,17 @@
+import torch.nn as nn
+
+class Model(nn.Module):
+    def __init__(self, encoder, head):
+        super().__init__()
+        self.encoder = encoder
+        self.head = head
+
+    def reset_parameters(self):
+        self.encoder.reset_parameters()
+        self.head.reset_parameters()
+
+    def forward(self, x):
+        return self.head(self.encoder(x))
+
+    def get_encoder_head(self):
+        return self.encoder, self.head
