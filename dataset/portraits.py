152fb2f142d026ff81cfd7a2aa10238f15063b8c
hsinghuan <hhchung@utexas.edu>
2023-04-04 15:07:20 -0500

    adopt the data partitioning method in the paper

--- dataset/portraits.py
+++ dataset/portraits.py
@@ -6,39 +6,32 @@ from torchvision import transforms
 from torchvision.io import read_image, ImageReadMode
 import matplotlib.pyplot as plt
 
-portraits_domains = [[1905,1940],[1940,1950],[1950,1960],[1960,1970],[1970,1980],[1980,1990],[1990,2000],[2000,2014]]
+
+
+src_num = 2000
+int_num = 12000
+tgt_num = 2000
+tgt_test_num = 1000
+interval = 2000
+portraits_domains = list(range(1 + int_num // interval + 1)) # source, intermediate domains, target
 
 class PortraitsDataset(Dataset):
-    def __init__(self, img_dir, domain, transform=None, target_transform=None, target_test=False):
-        self.img_dir = img_dir
+    def __init__(self, img_dir, transform=None, target_transform=None):
         self.transform = transform
         self.target_transform = target_transform
-        if domain == portraits_domains[-1]:
-            F_paths = []
-            M_paths = []
-            if target_test:
-                with open(os.path.join(img_dir, "F_target_test.txt"), "r") as f:
-                    for line in f:
-                        line = line.strip()
-                        F_paths.append(os.path.join(img_dir, line))
-                with open(os.path.join(img_dir, "M_target_test.txt"), "r") as f:
-                    for line in f:
-                        line = line.strip()
-                        M_paths.append(os.path.join(img_dir, line))
-            else:
-                with open(os.path.join(img_dir, "F_target_train.txt"), "r") as f:
-                    for line in f:
-                        line = line.strip()
-                        F_paths.append(os.path.join(img_dir, line))
-                with open(os.path.join(img_dir, "M_target_train.txt"), "r") as f:
-                    for line in f:
-                        line = line.strip()
-                        M_paths.append(os.path.join(img_dir, line))
-        else: # not the target domain
-            F_paths = [os.path.join(img_dir, "F", filename) for filename in os.listdir(os.path.join(img_dir, "F")) if int(filename[:4]) >= domain[0] and int(filename[:4]) < domain[1]]
-            M_paths = [os.path.join(img_dir, "M", filename) for filename in os.listdir(os.path.join(img_dir, "M")) if int(filename[:4]) >= domain[0] and int(filename[:4]) < domain[1]]
-        self.img_paths = F_paths + M_paths
-        self.img_labels = [0] * len(F_paths) + [1] * len(M_paths)
+
+        F_filenames = os.listdir(os.path.join(img_dir, "F"))
+        M_filenames = os.listdir(os.path.join(img_dir, "M"))
+        filenames = F_filenames + M_filenames
+        img_labels = [0] * len(F_filenames) + [1] * len(M_filenames)
+        filenames, img_labels = (list(t) for t in zip(*sorted(zip(filenames, img_labels))))
+        img_paths = []
+        for i in range(len(filenames)):
+            gender_str = "F" if img_labels[i] == 0 else "M"
+            img_paths.append(os.path.join(img_dir, gender_str, filenames[i]))
+
+        self.img_paths = img_paths
+        self.img_labels = img_labels
 
     def __len__(self):
         return len(self.img_labels)
@@ -70,14 +63,29 @@ def compute_portraits_stats(img_dir: str):
     return mean, std
 
 
-def get_portraits(data_dir: str, domain: List, batch_size: int, target_test: bool = False, val: bool = True):
+def get_portraits(data_dir: str, domain_idx: int, batch_size: int, target_test: bool = False, val: bool = True):
     transform = transforms.Compose([transforms.Normalize((128.8960,), (62.1401,)), transforms.Resize((128,128))])
-    dataset = PortraitsDataset(data_dir, domain, transform=transform, target_test=target_test)
+    dataset = PortraitsDataset(data_dir, transform=transform)
     if target_test:
-        assert domain == portraits_domains[-1]
+        assert domain_idx == len(portraits_domains) - 1
+        start_idx = src_num + int_num + tgt_num
+        end_idx = start_idx + tgt_test_num
+        print("start idx:", start_idx, "end idx:", end_idx)
+        dataset = torch.utils.data.Subset(dataset, range(start_idx, end_idx))
         test_loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=False)
         return test_loader
 
+    if domain_idx == 0:
+        start_idx = 0
+        end_idx = src_num
+    elif domain_idx == len(portraits_domains) - 1:
+        start_idx = src_num + int_num
+        end_idx = start_idx + tgt_num
+    else:
+        start_idx = src_num + (domain_idx - 1) * interval
+        end_idx = src_num + domain_idx * interval
+    print("start idx:", start_idx, "end idx:", end_idx)
+    dataset = torch.utils.data.Subset(dataset, range(start_idx, end_idx))
     if val:
         train_dataset, val_dataset = torch.utils.data.random_split(dataset, [int(len(dataset) * 0.9), len(dataset) - int(len(dataset) * 0.9)])
         train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
@@ -90,7 +98,7 @@ def get_portraits(data_dir: str, domain: List, batch_size: int, target_test: boo
 
 
 
-# dataset = PortraitsDataset("/home/hhchung/data/faces_aligned_small_mirrored_co_aligned_cropped_cleaned", portraits_domains[-1], target_test = True)
+# dataset = PortraitsDataset("/home/hhchung/data/faces_aligned_small_mirrored_co_aligned_cropped_cleaned", portraits_domains[0])
 # print(len(dataset))
 # print(dataset[3], dataset[3][0].shape)
 # plt.imshow(dataset[3][0].squeeze(0), cmap='gray')
@@ -99,7 +107,7 @@ def get_portraits(data_dir: str, domain: List, batch_size: int, target_test: boo
 # mean, std = compute_portraits_stats("/home/hhchung/data/faces_aligned_small_mirrored_co_aligned_cropped_cleaned")
 # print(mean, std)
 
-# train_loader, val_loader = get_portraits("/home/hhchung/data/faces_aligned_small_mirrored_co_aligned_cropped_cleaned", portraits_domains[0], 256)
+# train_loader, val_loader = get_portraits("/home/hhchung/data/faces_aligned_small_mirrored_co_aligned_cropped_cleaned", 0, 256)
 # print(len(train_loader))
 # print(len(val_loader))
 # for data, y in train_loader:
